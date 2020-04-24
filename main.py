3a6d607cb9a5bf7b7d945ddbe6e64c0c23c34aaa
Nolan Y? <ndymario@gmail.com>
2020-04-24 00:10:09 -0500

    Made a folder for the game's sprites, also fixed the class

--- main.py
+++ main.py
@@ -1,38 +1,49 @@
-#######################################################################
-#   Author: Nolan Y.                                                  #
-#                                                                     #
-#   Description: My goal is to recreate the New Super Mario Bros.     #
-#                DS gamemode Mario vs Luigi. Even if it's not perfect,#
-#                it'll be a fun challenge!                            #
-#######################################################################
+########################################################################
+#   Author: Nolan Y.                                                   #
+#                                                                      #
+#   Description: My goal is to recreate the New Super Mario Bros.      #
+#                DS gamemode Mario vs Luigi. Even if it's not perfect, #
+#                it'll be a fun challenge!                             #
+########################################################################
 
 # Import things I might need
 import pygame
+import sys
+from pygame.locals import *
+
+# Allows us to use another folder than the folder this file is located
+sys.path.insert(1, "./Sprites")
+
+##########--BEGIN CLASSES--##########
+
+# Class for the player
+class Player(object):
+    def __init__(self, player_skin = None, player_number = 0):
+        self.player_number = player_number
+        self.player_skin = player_skin
+        self.x = 100
+        self.y = 100
+        self.x_velocity = 0
+        self.y_velocity = 0
+
+##########--END CLASSES--##########
+
+# Inialize pygame stuff
 pygame.init()
+
+# Make the window title
+pygame.display.set_caption("Mario vs Luigi")
+
+# Create a player
+player = Player("Sprites/idle.png")
 
 # Define some constants
 SIZE = WIDTH, HEIGHT = 320, 240
-BLACK = 0, 0, 0
+BLACK = (0, 0, 0)
 
-# Setup the screen
+# Setup the screen and display stuff
 screen = pygame.display.set_mode(SIZE)
-
-# Make some classes
-
-# Class for the player
-def Player(object):
-    def __init__(self, player_skin, player_number = 0):
-        self.player_number = player_number
-        self.player_skin = player_skin
-        self.x_coord = x_coord
-        self.y_coord = y_coord
-        self.x_velocity = x_velocity
-        self.y_velocity = y_velocity
-
-        player_skin = ("idle.png")
-
-
-##########--END CLASSES--##########
+skin = pygame.image.load(player.player_skin)
 
 while True:
     for event in pygame.event.get():
@@ -41,5 +52,5 @@ while True:
     
     #Render the screen
     screen.fill(BLACK)
-    screen.blit(player)
+    screen.blit(skin, [player.x, player.y])
     pygame.display.flip()
