6f110310d897708622174a93eb0e0d05120263e7
Nolan Y? <ndymario@gmail.com>
2020-05-11 20:08:40 -0500

    Tile Collision (#9) (#10)
    * Contributor (#5)
    
    * extra thicc
    
    added luigi, best character, platform detection
    
    * Colision Update
    
    added x colision, fixed y colision
    
    * Fixed up code, organized to my "style"
    
    * Fixed player skin bug
    
    * Fixed collision, again
    
    Co-authored-by: Christopher <christopher.m.deck@gmail.com>
    
    * separated player class
    
    * Add a gitignore for cached files
    
    * bugs help
    
    this is for bbomb, everything is broken
    
    * Fixed Tile Collisions
    
    collision detection for the new level data system is fixed
    
    * Bottem of Tile Collision
    
    added collision to the bottom of tiles
    
    * Fixed All Collisions
    
    every angle of tiles has collisions
    
    Co-authored-by: Nolan Y? <ndymario@gmail.com>
    
    Co-authored-by: CMDisme <32135661+CMDisme@users.noreply.github.com>

--- level.py
+++ level.py
@@ -1,9 +1,5 @@
 ########################################################################
-<<<<<<< HEAD
 #   Author: Nolan Y. / bbomb64 / Christopher D.                        #
-=======
-#   Author: Nolan Y. / bbomb64                                         #
->>>>>>> 99f3c28ba75db2e0a9c4a3722fb0839d4a7d15ae
 #                                                                      #
 #   Description: Level/Tile handeler                                   #
 ########################################################################
@@ -21,13 +17,8 @@ class Tile(object):
         self.top = y - height
         self.width = width
         self.height = height
-<<<<<<< HEAD
-        self.left = x 
+        self.left = x
         self.right = x + width
-=======
-        self.left = x - (width/2)
-        self.right = x + (width/2)
->>>>>>> 99f3c28ba75db2e0a9c4a3722fb0839d4a7d15ae
 
     def __str__(self):
         return "Tile X Coord: {}\nTile Y Coord: {}".format(self.x, self.y)
@@ -58,11 +49,7 @@ class Level():
 
   def read_tiles(self):
     self.file.read(4) # do nothing with magic
-<<<<<<< HEAD
     
-=======
-
->>>>>>> 99f3c28ba75db2e0a9c4a3722fb0839d4a7d15ae
     # read all tiles in chunk
     at_end = False
     while not at_end:
@@ -77,16 +64,12 @@ class Level():
 
       for i in range(5):
         short = self.file.read(2)
-        if short == b'\xff\xff': 
+        if short == b'\xff\xff':
           at_end = True
           break
         tile[list(tile)[i]] = int.from_bytes(short,  byteorder='big')
       if not at_end:
-<<<<<<< HEAD
         self.tiles.append(Tile("Tiles/Grass_Top.png",1, tile["x"] * 16, tile["y"] * 16, tile["width"] * 16, tile["height"] * 16))
-=======
-        self.tiles.append(tile)
->>>>>>> 99f3c28ba75db2e0a9c4a3722fb0839d4a7d15ae
 
   def read_sprites(self):
     self.file.read(4) # do nothing with magic
@@ -103,31 +86,48 @@ class Level():
       
       for i in range(3):
         short = self.file.read(2)
-        if short == b'\xff\xff': 
+        if short == b'\xff\xff':
           at_end = True
           break
         sprite[list(sprite)[i]] = int.from_bytes(short,  byteorder='big')
       if not at_end:
         self.sprites.append(sprite)
-<<<<<<< HEAD
 
   def tile_on(self, x, y):
         for tile in self.tiles:
-            if (y  >= tile.top) and y < tile.top + 8 and x > tile.x - 18 and x < tile.x + tile.width: 
+            if (y  >= tile.top) and y <= tile.top + 8 and x > tile.x - 18 and x < tile.x + tile.width:
                 return tile
         return False
     
   def hit_under_tile(self, x, y,player):
         if player.y_velocity < 0:
             for tile in self.tiles:
-                if (y >= tile.top) and y < tile.y + 8 and x > tile.x - 18 and x < tile.x + tile.width: 
+                if (y > tile.y - 6) and y < tile.y and x > tile.x - 18 and x < tile.x + tile.width:
                     return tile
         return False
 
   def in_tile(self, x, y):
       for tile in self.tiles:
-          if (y > tile.top) and y <= tile.y and x <= tile.width + tile.x and x >=tile.x - 18:
+          if (y > tile.y - 8) and y <= tile.y - 1 and x < tile.width + tile.x and x >tile.x - 18:
               return tile
       return False
-=======
->>>>>>> 99f3c28ba75db2e0a9c4a3722fb0839d4a7d15ae
+
+  def lr_tile_collision(self, x, y, v):
+      for tile in self.tiles:
+          if (y <= tile.top + tile.height) and y > tile.top and x > tile.x - 18 and x <= tile.x - 17:
+              return tile.left - 18
+          if (y <= tile.top + tile.height) and y > tile.top and x >= tile.x + tile.width - 1 and x < tile.x + tile.width:
+              return tile.right
+          if (y < tile.top + tile.height-4) and y > tile.top and x + v > tile.x - 18 and x + v <= tile.x - 7:
+              return tile.left - 18
+          if (y < tile.top + tile.height-4) and y > tile.top and x + v >= tile.x + tile.width - 10 and x + v < tile.x + tile.width:
+              return tile.right
+      return False
+
+  def bottom_lr_tile_collision(self,x,y):
+      for tile in self.tiles:
+          if (y <= tile.top + tile.height) and y > tile.top and x > tile.x - 18 and x <= tile.x - 8:
+                  return tile.left - 18
+          if (y <= tile.top + tile.height) and y > tile.top and x >= tile.x + tile.width - 10 and x < tile.x + tile.width:
+                  return tile.right
+      return False
--- main.py
+++ main.py
@@ -17,114 +17,12 @@ from player import *
 # Allows us to use another folder than the folder this file is located
 sys.path.insert(1, "./Sprites")
 
-<<<<<<< HEAD
 #---------------------------------#
 ##########--BEGIN CLASSES--##########
-=======
-levelchunk1 = []
-
-level = Level("Levels/1-1.lvl")
-
-for tile in level.tiles:
-    levelchunk1.append(Tile("Tiles/Grass_Top.png", 1, tile["x"] * 16, tile["y"] * 16, tile["width"] * 16, tile["height"] * 16))
-
-##########--BEGIN CLASSES--##########
-
-# Class for the player
-class Player(object):
-    def __init__(self, skin = None, height = 0, weight = 0.2, player_number = 0):
-        self.player_number = player_number
-        # Used to determine what sprites to load for the player
-        self.skin = skin
-        self.x = 100
-        self.y = 100
-        self.x_velocity = 0.00
-        self.y_velocity = 0.00
-        # Number is from height of the player sprite in pixles
-        self.height = height
-        self.weight = weight
-    
-    def gravity(self, gravity):
-        p_weight = self.weight
-        if ((player.y_velocity >= VSPEED_CAP) and (player.y_velocity < 0)):
-            if p_weight <= gravity:
-                    p_weight += gravity
-                    
-            else:
-                p_weight = gravity
-            player.y_velocity += (player.weight * p_weight)
-
-            if player.y_velocity >= 0:
-                player.y_velocity = 0.0
-
-        elif((player.y_velocity >= 0.0) and (player.check_fall() ==False)):
-            if p_weight <= gravity:
-                p_weight += gravity
-            else:
-                p_weight = gravity
-
-            player.y_velocity += (player.weight * p_weight)
-            if player.y_velocity < VSPEED_CAP:
-                player.y_velocity = VSPEED_CAP
-
-        else:
-            if player.check_fall() != False:
-                player.y_velocity = 0.0
-                player.y = player.check_fall()
-    
-    def death(self):
-        pass
-
-    def respawn(self):
-        pass
-    
-    def check_jump(self):
-        for tile in levelchunk1:
-            if (player.y == tile.top) and player.x <= tile.width + tile.x and player.x >=tile.x - tile.x: 
-                return True
-        return False
-
-    def check_fall(self):
-        for tile in levelchunk1:
-            if (player.y >= tile.top) and player.y <= tile.top + 10 and player.x <= tile.width + tile.x and player.x >=tile.x - 16: 
-                return tile.top
-        return False
-
-    def calculatePosition(self):
-        # Make it so the player wraps around on the left and right (if enabled)
-        if (wrap_around):
-            if (((player.x >= WIDTH - 10) and (player.x <= WIDTH)) and player.x_velocity >= 0):
-                player.x = 11
-            elif ((player.x >= 0) and (player.x <= 10)) and (player.x_velocity <= 0):
-                player.x = WIDTH - 11
-        
-        # Calculate the players next position using their coordinates
-        # (This will probably be improved in the future)
-        self.x += self.x_velocity
-        self.y += self.y_velocity
-
-    
-    # Print stats of the player when called
-    def __str__(self):
-        return "Player X Velocity: {}\nPlayer Y Velocity: {}\nPlayer X: {}\nPlayer Y: {}"\
-            .format(player.x_velocity, player.y_velocity, player.x, player.y)
->>>>>>> 99f3c28ba75db2e0a9c4a3722fb0839d4a7d15ae
   
 ##########--END CLASSES--##########
 #---------------------------------#
 ##########--BEING FUNCTIONS--######
-<<<<<<< HEAD
-=======
-
-def check_colision():
-    for tile in levelchunk1:
-        if (player.x >= tile.left -11) and player.x <= tile.right + 8 and player.y -1 > tile.top and player.y - 1 <=tile.y: 
-            if player.x - tile.left-11 < tile.right - player.x + 3:
-                return tile.left -11
-            else:
-                return tile.right + 8
-    return False
->>>>>>> 99f3c28ba75db2e0a9c4a3722fb0839d4a7d15ae
   
 ##########--END FUNCTIONS--########
 #---------------------------------#
@@ -160,7 +58,7 @@ frame = 1
 
 # Create a player
 player = Player("Sprites/idle.png",18)
-playerL = Player("Sprites/idle.png",20)
+playerL = Player("Sprites/idle.png",18)
 players = [player,playerL]
 
 # Define player controls
@@ -178,26 +76,28 @@ while True:
 
     # Generate player x velocity
     if keys[right]:
-        # Cap the player's horizontal speed to the right
-        if (player.x_velocity <= SPEED_CAP):
-            player.x_velocity += ACCELERATION
-            if player.x_velocity >= SPEED_CAP:
+        if player.check_colision(level) == False:
+            # Cap the player's horizontal speed to the right
+            if (player.x_velocity <= SPEED_CAP):
+                player.x_velocity += ACCELERATION
+                if player.x_velocity >= SPEED_CAP:
+                    player.x_velocity = SPEED_CAP
+            elif (player.x_velocity >= SPEED_CAP):
                 player.x_velocity = SPEED_CAP
-        elif (player.x_velocity >= SPEED_CAP):
-            player.x_velocity = SPEED_CAP
  
             
     elif keys[left]:
-        # Cap the player's horizontal speed to the left
-        if (player.x_velocity >= -SPEED_CAP):
-            player.x_velocity -= ACCELERATION
-            if player.x_velocity <= -SPEED_CAP:
+        if player.check_colision(level) == False:
+            # Cap the player's horizontal speed to the left
+            if (player.x_velocity >= -SPEED_CAP):
+                player.x_velocity -= ACCELERATION
+                if player.x_velocity <= -SPEED_CAP:
+                    player.x_velocity = -SPEED_CAP
+            elif (player.x_velocity <= -SPEED_CAP):
                 player.x_velocity = -SPEED_CAP
-        elif (player.x_velocity <= -SPEED_CAP):
-            player.x_velocity = -SPEED_CAP
             
     # Apply friction to the player if they are not holding a button (slow them to a hault)
-    else:          
+    else:
         if ((player.x_velocity <= SPEED_CAP) and (player.x_velocity > 0)):
             if ((player.x_velocity <= SPEED_CAP) and (player.x_velocity > 0)):
                 player.x_velocity -= FRICTION
@@ -237,31 +137,28 @@ while True:
     clock.tick(60)
 
     player.calculatePosition()
-    if level.hit_under_tile(player.x,player.y - 33,player) != False:
-        player.y = level.hit_under_tile(player.x,player.y - 33,player).y + 33
+    if level.hit_under_tile(player.x,player.y - 20,player) != False:
+        player.y = level.hit_under_tile(player.x,player.y - 20,player).y + 20
         player.y_velocity = 0.0
-    if player.check_fall(level) != False:
-        player.y = player.check_fall(level)
+    if level.tile_on(player.x,player.y) != False:
+        player.y = level.tile_on(player.x,player.y).top
         player.y_velocity = 0.0
     if player.check_colision(level) != False:
         player.x = player.check_colision(level)
         player.x_velocity = 0.0
+    if player.check_fall(level) != False:
+        player.y = player.check_fall(level)
+        player.y_velocity = 0.0
+    if level.bottom_lr_tile_collision(player.x,player.y) != False:
+        player.x = level.bottom_lr_tile_collision(player.x,player.y)
+        player.x_velocity = 0.0
+    
+    
     #Render the screen
     screen.fill(BLACK)
-<<<<<<< HEAD
     for tile in level.tiles:
         for w in range(int(tile.width / 16)):
             for h in range(int(tile.height / 16)):
-=======
-    for tile in levelchunk1:
-        for w in range(tile.width // 16):
-            for h in range(tile.height // 16):
->>>>>>> 99f3c28ba75db2e0a9c4a3722fb0839d4a7d15ae
-                screen.blit(pygame.image.load(tile.tile_image), [tile.x + (w * 16), tile.y + (h * 16)])
+                screen.blit(pygame.image.load(tile.tile_image), [tile.x + (w * 16), tile.y - (h * 16)])
     screen.blit(pygame.image.load(player.skin), [player.x, player.y - player.height])
     pygame.display.flip()
-
-
-
-
-
