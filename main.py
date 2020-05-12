583da20e390283de94f134bf42438f5f0c1b6eb7
Nolan Y? <ndymario@gmail.com>
2020-05-12 18:45:18 -0500

    Mario's sprite will now flip depending on the last direction held

--- main.py
+++ main.py
@@ -67,6 +67,9 @@ right = pygame.K_RIGHT
 # Display Sprites as they're needed
 showSprite(playerSprite)
 
+# Define misc. variables
+last_held_direction = "right"
+
 while True:
     events = pygame.event.get()
     for event in events:
@@ -74,8 +77,18 @@ while True:
 
     keys = pygame.key.get_pressed()
 
+    # Update the player's sprite when idling
+    if (player.check_jump(level) == True):
+        # If the last input was to the right, face Mario's sprite to the right
+        if (last_held_direction == "right"):
+            changeSpriteImage(playerSprite, 3*3+1)
+        # Otherwise, face Mario's sprite to the left
+        else:
+            changeSpriteImage(playerSprite, 0*3+1)
+
     # Generate player x velocity
     if keys[right]:
+        last_held_direction = "right"
         # Update the player's sprite when walking
         changeSpriteImage(playerSprite, 3*3+frame)
         if player.check_colision(level) == False:
@@ -89,6 +102,7 @@ while True:
  
             
     elif keys[left]:
+        last_held_direction = "left"
         # Update the player's sprite when walking
         changeSpriteImage(playerSprite, 0*3+frame)
         if player.check_colision(level) == False:
@@ -102,8 +116,6 @@ while True:
             
     # Apply friction to the player if they are not holding a button (slow them to a hault)
     else:
-        # Update the player's sprite when idling
-        #changeSpriteImage(playerSprite, 0*3+1)
         if ((player.x_velocity <= SPEED_CAP) and (player.x_velocity > 0)):
             if ((player.x_velocity <= SPEED_CAP) and (player.x_velocity > 0)):
                 player.x_velocity -= FRICTION
@@ -124,17 +136,37 @@ while True:
     # Check to see if the player can jump
     if keys[up]:
         if player.check_jump(level) == True:
-            # Update the player's sprite when jumping
-            changeSpriteImage(playerSprite, 1*3+frame)
+            # If the last input was to the right, face Mario's sprite to the right
+            if (last_held_direction == "right"):
+                changeSpriteImage(playerSprite, 2*3+frame)
+            # Otherwise, face Mario's sprite to the left
+            else:
+                changeSpriteImage(playerSprite, 1*3+frame)
+            
             if (player.y_velocity > VSPEED_CAP):
                 player.y_velocity = VSPEED_CAP
             elif (player.y_velocity < VSPEED_CAP):
                 player.y_velocity = VSPEED_CAP
         else:
+            # If the last input was to the right, face Mario's sprite to the right
+            if (last_held_direction == "right"):
+                changeSpriteImage(playerSprite, 2*3+frame)
+            # Otherwise, face Mario's sprite to the left
+            else:
+                changeSpriteImage(playerSprite, 1*3+frame)
             # If the player can't jump, continue to apply gravity
             player.gravity(GRAVITY,level)
 
     # Apply gravity to the player
+    elif (player.check_jump(level) == False):
+        # If the last input was to the right, face Mario's sprite to the right
+        if (last_held_direction == "right"):
+            changeSpriteImage(playerSprite, 2*3+frame)
+        # Otherwise, face Mario's sprite to the left
+        else:
+            changeSpriteImage(playerSprite, 1*3+frame)
+        player.gravity(GRAVITY,level)
+
     else:
         player.gravity(GRAVITY,level)
     
@@ -174,4 +206,4 @@ while True:
     # Limits the frame rate of sprites (60 FPS walk cycle is bad)
     if clock() > nextFrame:
         frame = (frame+1)%3
-        nextFrame += 80
+        nextFrame += 60
