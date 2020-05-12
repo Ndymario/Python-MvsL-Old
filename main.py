fa5d8072abce33d1753d776c59d92a1252b6c957
Nolan Y? <ndymario@gmail.com>
2020-05-12 15:43:39 -0500

    Added a beta Mario animation for walking and jumping! Lags the game, needs to be investigated

--- main.py
+++ main.py
@@ -6,10 +6,10 @@
 ########################################################################
 
 # Enable/Disable DEBUG mode
-DEBUG = False
+DEBUG = True
 
 # Import things I might need
-import pygame
+from pygame_functions import *
 import sys
 from level import *
 from player import *
@@ -32,10 +32,6 @@ sys.path.insert(1, "./Sprites")
 levelchunk1 = []
 level = Level("Levels/1-1.lvl")
 
-# Inialize pygame stuff
-pygame.init()
-clock = pygame.time.Clock()
-
 # Make the window title
 pygame.display.set_caption("Mario vs Luigi")
 
@@ -43,7 +39,8 @@ pygame.display.set_caption("Mario vs Luigi")
 BLACK = (0, 0, 0)
 
 # Setup the screen and other display stuff
-screen = pygame.display.set_mode(SIZE)
+# Note: WIDTH & HEIGHT are imported from player.py!
+screen = screenSize(WIDTH, HEIGHT)
 
 # Define some in game constants (used for the Physics "engine")
 SPEED_CAP = 8.0
@@ -53,19 +50,22 @@ ACCELERATION = 0.1
 V_ACCELERATION = 0.1
 GRAVITY = 2.5
 
-# Define misc variables
-frame = 1
+# Frame handler (used for any sprite animation)
+frame = 0
+nextFrame = clock()
 
 # Create a player
-player = Player("Sprites/idle.png",18)
-playerL = Player("Sprites/idle.png",18)
-players = [player,playerL]
+player = Player("Sprites/Mario.png",0)
+playerSprite = makeSprite(player.skin, 15)
 
 # Define player controls
 up = pygame.K_UP
 down = pygame.K_DOWN
 left = pygame.K_LEFT
 right = pygame.K_RIGHT
+
+# Display Sprites as they're needed
+showSprite(playerSprite)
 
 while True:
     events = pygame.event.get()
@@ -76,6 +76,8 @@ while True:
 
     # Generate player x velocity
     if keys[right]:
+        # Update the player's sprite when walking
+        changeSpriteImage(playerSprite, 3*3+frame)
         if player.check_colision(level) == False:
             # Cap the player's horizontal speed to the right
             if (player.x_velocity <= SPEED_CAP):
@@ -87,6 +89,8 @@ while True:
  
             
     elif keys[left]:
+        # Update the player's sprite when walking
+        changeSpriteImage(playerSprite, 0*3+frame)
         if player.check_colision(level) == False:
             # Cap the player's horizontal speed to the left
             if (player.x_velocity >= -SPEED_CAP):
@@ -98,6 +102,8 @@ while True:
             
     # Apply friction to the player if they are not holding a button (slow them to a hault)
     else:
+        # Update the player's sprite when idling
+        #changeSpriteImage(playerSprite, 0*3+1)
         if ((player.x_velocity <= SPEED_CAP) and (player.x_velocity > 0)):
             if ((player.x_velocity <= SPEED_CAP) and (player.x_velocity > 0)):
                 player.x_velocity -= FRICTION
@@ -118,6 +124,8 @@ while True:
     # Check to see if the player can jump
     if keys[up]:
         if player.check_jump(level) == True:
+            # Update the player's sprite when jumping
+            changeSpriteImage(playerSprite, 1*3+frame)
             if (player.y_velocity > VSPEED_CAP):
                 player.y_velocity = VSPEED_CAP
             elif (player.y_velocity < VSPEED_CAP):
@@ -134,7 +142,7 @@ while True:
         print(player)
 
     # Limit the framerate to 60 FPS
-    clock.tick(60)
+    tick(60)
 
     player.calculatePosition()
     if level.hit_under_tile(player.x,player.y - 20,player) != False:
@@ -160,5 +168,9 @@ while True:
         for w in range(int(tile.width / 16)):
             for h in range(int(tile.height / 16)):
                 screen.blit(pygame.image.load(tile.tile_image), [tile.x + (w * 16), tile.y - (h * 16)])
-    screen.blit(pygame.image.load(player.skin), [player.x, player.y - player.height])
-    pygame.display.flip()
+    moveSprite(playerSprite, player.x, player.y, True)
+
+    # Limits the frame rate of sprites (60 FPS walk cycle is bad)
+    if clock() > nextFrame:
+        frame = (frame+1)%3
+        nextFrame += 80
